08e01d239833d7d39cf7881d7070b1f90150e5c4
xtian08 <chriskint@yahoo.com>
2023-05-01 15:27:55 +0400

    Create nomad.sh

---
+++ nomad.sh
@@ -0,0 +1,42 @@
+#NoMAD Prefereces
+
+# Preference key reference
+# https://gitlab.com/orchardandgrove-oss/NoMADLogin-AD/wikis/Configuration/preferences
+domain="ad.nyu.edu"
+background_image="/usr/local/nomadback.jpeg"
+#logo="/usr/local/nomadlogo.png"
+
+# Set default AD domain
+defaults write /Library/Preferences/menu.nomad.login.ad.plist ADDomain "$domain"
+
+# Create user as Admin pref
+defaults write /Library/Preferences/menu.nomad.login.ad.plist CreateAdminUser -bool Yes
+
+# Set background image
+defaults write /Library/Preferences/menu.nomad.login.ad.plist BackgroundImage "$background_image"
+
+# Set login window logo
+defaults write /Library/Preferences/menu.nomad.login.ad.plist LoginLogo "$logo"
+
+# Set security authorization database mechanisms with authchanger
+/usr/local/bin/authchanger -reset -AD
+
+#Install Nomad
+
+echo "Installing Latest NoMAD" 
+cd /tmp
+curl -L -o nomadL.pkg https://files.nomad.menu/NoMAD-Login-AD.pkg
+echo "Installing NoMAD" >> $ws1log
+sudo -S installer -allowUntrusted -pkg "/tmp/nomadL.pkg" -target /;
+
+        if [[ $? -eq 0 ]]; then
+        	echo "NoMAD has been successfully installed."
+        else
+        	echo "NoMAD installation failed!"
+        	exitcode=1
+        fi
+
+#Invoke Nomad UI
+# Kill loginwindow process to force NoMAD Login to launch
+#/usr/bin/killall -HUP loginwindow
+echo "Nomand Login Loaded"
