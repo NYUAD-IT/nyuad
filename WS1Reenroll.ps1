31cf9fd44783d7479aaa075c81f7b93e0ba88d72
xtian08 <chriskint@yahoo.com>
2023-10-18 17:24:02 +0400

    Update WS1Reenroll.ps1

--- WS1Reenroll.ps1
+++ WS1Reenroll.ps1
@@ -80,7 +80,7 @@ param (
 		$true
 	} else {throw "$_ is invalid. Specify Always, OnSIDMismatch, or Never"}
 })]
-	$Unenroll
+	$Unenroll = "OnSIDMismatch"
 
 )
 #-----------------PSADT Function "QueryUser" from AppDeployToolkitMain.cs file------------------------------------
